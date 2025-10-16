d8fa57cc9b9b39ee72e3f8bfcd8497fea788609f
JorgeHernandez09 <jorgehernandezm2004@gmail.com>
2025-10-16 17:42:28 +0200

    Ejercicios 10 y 12 : Composables reutilizables y vue computed

--- NUXT/nuxt-vue-exercises/app/composables/useCounter.ts
+++ NUXT/nuxt-vue-exercises/app/composables/useCounter.ts
@@ -4,6 +4,8 @@ export function useCounter(num = 0, upLimit = 10, downLimit = 0) {  // Parámetr
   const countLimitDown = ref(downLimit);
   const Increment = () => count.value++;
   const Decrement = () => count.value--;
+  const multipliedCount = computed(() => count.value * 2);
+  const Reset = () => count.value = 0;
 
-  return { count, countLimitUp, countLimitDown, Increment, Decrement };
+  return { count, countLimitUp, countLimitDown, Increment, Decrement, multipliedCount };
 }
