5525612e3f427c5bb48a1b57c3036ac6113a9b6e
JorgeHernandez09 <jorgehernandezm2004@gmail.com>
2025-10-16 13:14:51 +0200

    Ejercicios 4 y 5: Contador y Directivas condicionales

--- NUXT/nuxt-vue-exercises/app/components/TheCounter.vue
+++ NUXT/nuxt-vue-exercises/app/components/TheCounter.vue
@@ -1,23 +1,22 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
-
-const count = ref(0)
-
+const count = ref(0);
 const countLimitUp = 10;
 const countLimitDown = 0;
-const Increment = ()=> count.value++;
-const Decrement = ()=> count.value--;
-
+const Increment = () => count.value++;
+const Decrement = () => count.value--;
 </script>
 
 <template>
-<div>
-    <button class=button v-if="count >countLimitDown" @click="Decrement">Decrementar</button>
-    <button class=button v-if="count <countLimitUp" @click="Increment">Incrementar</button>
+  <div>
+    <button class="button" v-if="count > countLimitDown" @click="Decrement">
+      Decrementar
+    </button>
+    <button class="button" v-if="count < countLimitUp" @click="Increment">
+      Incrementar
+    </button>
 
     <TheTitle>
-        <p>El numero es: {{ count }}</p>
+      <p>El numero es: {{ count }}</p>
     </TheTitle>
-
-    </div>
-</template> 
+  </div>
+</template>
